24ca319fc8bf0fb8cc5d06f033ef331b4e94ab2a
Mairead Meagher <mmeagher@wit.ie>
2026-09-07 16:01:40 +0100

    change calendar

---
+++ topic-01-data-types/unit-01a-data-types/book-1-data-types/archives/break.py
@@ -0,0 +1,11 @@
+# Different data types in Python
+
+name = "Siobhan"          # str
+age = 39                # int
+height = 1.48           # float
+is_student = True       # bool
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is a student:", is_student)
